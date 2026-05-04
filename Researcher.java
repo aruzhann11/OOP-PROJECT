e68500087884e26618b72072c1c907a4fac50199
webuser <nurbek120105@gmail.com>
2026-05-04 11:30:32 +0500

    Modified classes Researcher, ResearchPaper, ResearchProject

--- Researcher.java
+++ Researcher.java
@@ -1,3 +1,7 @@
-interface  Researcher {
-    
+import java.util.List;
+
+public interface Researcher {
+    void addPaper(ResearchPaper paper);
+    List<ResearchPaper> getPapers();
+    void printPapers();
 }
